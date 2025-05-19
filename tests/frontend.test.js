795c32ec0193ed82e09194a40cbb32bc8db03a20
Nguyễn Triêu Vỹ <2000000740@nttu.edu.vn>
2025-05-19 15:50:36 +0700

    Update frontend.test.js

--- tests/frontend.test.js
+++ tests/frontend.test.js
@@ -1,6 +1,3 @@
-module.exports = {
-  testMatch: [
-    "**/my-custom-directory/**/*.[jt]s?(x)", // Thay đổi đường dẫn này theo ý bạn
-    "**/?(*.)+(spec|test).[jt]s?(x)"
-  ],
-};
+test('basic test', () => {
+  expect(true).toBe(true);  // A simple test case that always passes
+});
