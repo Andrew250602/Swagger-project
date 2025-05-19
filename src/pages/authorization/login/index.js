105d01aed921947b46038ea2cdd3fbab75b91c8c
TrieuVyx <trieuvy.nguyen@vn.wilmar-intl.com>
2025-05-19 08:11:08 +0700

    add login form

--- src/pages/authorization/login/index.js
+++ src/pages/authorization/login/index.js
@@ -1,30 +1,41 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../../../redux/action/user';
-
+import "../../../style/author/index.scss"
 const LoginComponent = (props) => {
     const { fetchUsers, message, users, loading, error, displayMessage } = props;
 
     useEffect(() => {
-        fetchUsers(); 
-    }, [fetchUsers]); // Only depend on fetchUsers
+        fetchUsers();
+    }, [fetchUsers]);
 
     return (
-        <div>
-            <h1>Welcome to the Outlet!</h1>
-            <p style={{ color: "red" }}>{message}</p>
-            <button onClick={fetchUsers}>Click Me!</button>
+        <>
+            <div class="login-wrapper">
+                <div class="login-header">
+                    <h2>
+                        <span>▶</span> LOGIN <span>❤</span>
+                    </h2>
+                </div>
+                <div class="login-form">
+                    <div class="input-group">
+                        <input type="text" placeholder="Username" />
+                    </div>
+                    <div class="input-group">
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <button class="sign-in-button">Sign in</button>
+                    <div class="links">
+                        <a href="#">Forgot Password</a>
+                        <a href="#">Sign up</a>
+                    </div>
+                </div>
+            </div>
 
-            {loading && <p>Loading users...</p>}
-            {error && <p style={{ color: "red" }}>Error: {error}</p>}
-            <ul>
-                {users && users.map(user => (
-                    <li key={user.id}>{user.name}</li>
-                ))}
-            </ul>
-        </div>
+        </>
     );
 };
+
 
 // Function to map dispatch to props
 const mapDispatchToProps = (dispatch) => ({
@@ -43,3 +54,18 @@ const mapStateToProps = (state) => {
 
 // Connect the component to Redux
 export default connect(mapStateToProps, mapDispatchToProps)(LoginComponent);
+
+
+// <div>
+//     <h1>Welcome to the Outlet!</h1>
+//     <p style={{ color: "red" }}>{message}</p>
+//     <button onClick={fetchUsers}>Click Me!</button>
+
+//     {loading && <p>Loading users...</p>}
+//     {error && <p style={{ color: "red" }}>Error: {error}</p>}
+//     <ul>
+//         {users && users.map(user => (
+//             <li key={user.id}>{user.name}</li>
+//         ))}
+//     </ul>
+// </div>
--- src/redux/storeReducer.js
+++ src/redux/storeReducer.js
@@ -1,9 +1,7 @@
-// src/store.js
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
-
+import  thunk  from 'redux-thunk';
 const composedEnhancer = composeWithDevTools(
     applyMiddleware(thunk)
 );
